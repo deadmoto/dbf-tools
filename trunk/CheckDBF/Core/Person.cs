f27bd50277dd89612078540ee80617c485c64bff
deadmoto <deadmoto@1d78cd27-8f3f-8867-ac35-44f68c465715>
2010-12-13 09:49:14 +0000

    [CheckDBF] Extracted Person and Service fields. Implemented error file output.

--- trunk/CheckDBF/Core/Person.cs
+++ trunk/CheckDBF/Core/Person.cs
@@ -1,36 +1,9 @@
 ﻿using System;
 namespace CheckDBF.Core
 {
-    class Person
+    class Person : PersonData
     {
-        public string FAMIL;
-        public string IMJA;
-        public string OTCH;
-        public DateTime DROG;
-        public string NPSS;
-        public string PY;
-        public string NNASP;
-        public string NYLIC;
-        public int NDOM;
-        public string LDOM;
-        public int KORP;
-        public int NKW;
-        public string LKW;
-        public int PVID;
-        public string PSR;
-        public string PNM;
-        public int KSS;
-        public string KOD;
-        public DateTime SROKS;
-        public DateTime SROKPO;
-        public int KDOMVL;
-        public double ROPL;
-        public int KCHLS;
-        public int K_POL;
-        public int KKOM;
-        public DateTime DATE_VIGR;
-        public string PRIM;
-        public Service[] Services = new Service[9];
+
 
         public bool GetErrorEMPTY()
         {
@@ -202,7 +175,7 @@ namespace CheckDBF.Core
             return 0;
         }
 
-        public string GetCommandText(string FileName)
+        public string GetCommandText(string FileName, bool Valid)
         {
             string CommandText = "INSERT INTO '" + FileName + "' VALUES ";
             CommandText += "('" + FAMIL;
@@ -233,7 +206,7 @@ namespace CheckDBF.Core
 
             for (int i = 0; i < 9; i++)
             {
-                CommandText += "," + Services[i].GetCommandText(ROPL, KCHLS);
+                CommandText += "," + Services[i].GetCommandText(ROPL, KCHLS, Valid);
             }
 
             CommandText += "," + DATE_VIGR.ToString("{^yyyy/MM/dd}");
