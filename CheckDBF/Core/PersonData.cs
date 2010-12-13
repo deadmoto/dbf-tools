fa8eee85ca0b3e5e321dda7302ba26e7cc71997d
deadmoto <deadmoto@1d78cd27-8f3f-8867-ac35-44f68c465715>
2010-12-13 09:46:41 +0000

    [CheckDBF] Added parent classes to extract Person and Service fields.

---
+++ CheckDBF/Core/PersonData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CheckDBF.Core
+{
+    class PersonData
+    {
+        public string FAMIL;
+        public string IMJA;
+        public string OTCH;
+        public DateTime DROG;
+        public string NPSS;
+        public string PY;
+        public string NNASP;
+        public string NYLIC;
+        public int NDOM;
+        public string LDOM;
+        public int KORP;
+        public int NKW;
+        public string LKW;
+        public int PVID;
+        public string PSR;
+        public string PNM;
+        public int KSS;
+        public string KOD;
+        public DateTime SROKS;
+        public DateTime SROKPO;
+        public int KDOMVL;
+        public double ROPL;
+        public int KCHLS;
+        public int K_POL;
+        public int KKOM;
+        public DateTime DATE_VIGR;
+        public string PRIM;
+        public Service[] Services = new Service[9];
+    }
+}
